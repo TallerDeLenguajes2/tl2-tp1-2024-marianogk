94030c8a7d8cd994b4955e508c56586207541b8b
marianogk <marianokpo7@gmail.com>
2024-08-23 19:26:05 -0300

    generando constructores

--- Program.cs
+++ Program.cs
@@ -6,6 +6,11 @@
         private string direccion;
         private int telefono;
         private string datosReferenciaDireccion;
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Direccion { get => direccion; set => direccion = value; }
+        public int Telefono { get => telefono; set => telefono = value; }
+        public string DatosReferenciaDireccion { get => datosReferenciaDireccion; set => datosReferenciaDireccion = value; }
     }
     enum estado
     {
@@ -17,6 +22,12 @@
     private int nro;
     private string obs;
     private Cliente cliente;
+
+    public int Nro { get => nro; set => nro = value; }
+    public string Obs { get => obs; set => obs = value; }
+
+
+
     private enum estado;
 }
 class Cadeteria
@@ -24,14 +35,24 @@ class Cadeteria
     private string nombre;
     private int telefono;
     private List<Cadete> listadoCadetes
+public string Nombre { get => nombre; set => nombre = value; }
+    public int Telefono { get => telefono; set => telefono = value; }
+
     class Cadete
     {
         private int id;
         private string nombre;
         private string direccion;
         private int telefono;
-        private List<int> listadoPedidos;
+        private List<int> listadoPedidos = public Cadete()
+        {
+            listadoPedidos = new List<Pedido>();
+        }
 
+        public int Id { get => id; set => id = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Direccion { get => direccion; set => direccion = value; }
+        public int Telefono { get => telefono; set => telefono = value; }
     }
 }
 
