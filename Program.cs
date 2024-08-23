c70179d12de82405bc4d66e1835910626dd2d354
marianogk <marianokpo7@gmail.com>
2024-08-23 18:37:03 -0300

    creando clases con sus campos

--- Program.cs
+++ Program.cs
@@ -1,16 +1,34 @@
-﻿class Cadete
-{
-    
-}
-class Cadeteria
-{
-    
-}
-
-class Pedido
+﻿class Pedido
 {
     class Cliente
     {
+        private string nombre;
+        private string direccion;
+        private int telefono;
+        private string ref datosReferenciaDireccion;
+    }
+    enum estado
+    {
+
+    }
+    private int nro;
+    private string obs;
+    private Cliente cliente;
+    private enum estado;
+}
+class Cadeteria
+{
+    private string nombre;
+    private int telefono;
+    private List<Cadete> listadoCadetes
+    class Cadete
+    {
+        private int id;
+        private string nombre;
+        private string direccion;
+        private int telefono;
+        private List<int> listadoPedidos;
 
     }
 }
+
